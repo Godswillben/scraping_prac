f23287041935639903ea544990747f7606389371
Godswillben <amogugodswill99@gmail.com>
2023-07-22 09:47:44 +0100

    basics

--- 1/main.py
+++ 1/main.py
@@ -5,7 +5,9 @@ populations = [39613493, 29730311, 21944577, 19299981]
 
 dict_states = {'States':states, 'Population':populations}
 
-df_states = pd.DataFrame.from_dict
+df_states = pd.DataFrame.from_dict(dict_states)
+# print(df_states)
+df_states.to_csv('states.csv')
 
-with open("test.json", 'w') as file:
-    file.write('{"msg":"Data successfully scraped!", "age":1}')
+# with open("test.json", 'w') as file:
+#     file.write('{"msg":"Data successfully scraped!", "age":1}')
