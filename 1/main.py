3ce89f7ac75bddb65b04a055c2abf0bdd96c3356
Godswillben <amogugodswill99@gmail.com>
2023-07-22 09:13:07 +0100

    basics

--- 1/main.py
+++ 1/main.py
@@ -1,5 +1,11 @@
-states = ["California", "Texas", "Florida", "New York"]
+import pandas as pd
 
+states = ["California", "Texas", "Florida", "New York"]
+populations = [39613493, 29730311, 21944577, 19299981]
+
+dict_states = {'States':states, 'Population':populations}
+
+df_states = pd.DataFrame.from_dict
 
 with open("test.json", 'w') as file:
     file.write('{"msg":"Data successfully scraped!", "age":1}')
