0c8d0e5f2803c52575c2ba848660918a144c02a0
Godswillben <amogugodswill99@gmail.com>
2023-07-23 22:49:43 +0100

    beautiful soup

--- 1/error.py
+++ 1/error.py
@@ -1,8 +1,13 @@
-from bs4 import BeautifulSoup
-import requests
+import pandas as pd
 
-website = "https://subslikescript.com/movie/Titanic-120338"
-result = requests.get(website)
-content = result.text
+states = ["California", "Texas", "Florida", "New York"]
+populations = [39613493, 29730311, 21944577, 19299981]
 
-soup = 
+dict_states = {'States':states, 'Population':populations}
+
+df_states = pd.DataFrame.from_dict(dict_states)
+# print(df_states)
+df_states.to_csv('states.csv')
+
+# with open("test.json", 'w') as file:
+#     file.write('{"msg":"Data successfully scraped!", "age":1}')
