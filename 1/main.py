3428ef52f00e68aeba213eb20fbfbdcae985a0b6
Godswillben <amogugodswill99@gmail.com>
2023-07-24 17:52:56 +0100

    beautiful soup

--- 1/main.py
+++ 1/main.py
@@ -12,6 +12,7 @@ box = soup.find('article', class_='main-article')
 
 title = box.find('h1').get_text()
 
-transcript = box.find('div', class_='full-script').get_text()
+transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
+
 print(title)
 print(transcript)
